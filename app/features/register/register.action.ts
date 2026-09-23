e89702a8d068d876dd0a6aea389902ac9f804cec
Jesus Ponce <j.emmanuel.ponce.b@gmail.com>
2026-09-22 23:54:07 -0600

    feat: add password hash and send

--- app/features/register/register.action.ts
+++ app/features/register/register.action.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import db from "@/db/drizzle";
+import { hash } from "bcryptjs";
 import { formSchema, RegisterFormValues } from "./register.schema";
+import { users } from "@/db/userSchema";
 
 export const registerAction = async (data: RegisterFormValues) => {
   const userValidation = formSchema.safeParse(data);
@@ -11,4 +14,11 @@ export const registerAction = async (data: RegisterFormValues) => {
       message: userValidation.error.issues[0]?.message ?? "An error occurred",
     };
   }
+
+  const hashPassword = await hash(data.password, 10);
+
+  await db.insert(users).values({
+    email: data.email,
+    password: hashPassword,
+  });
 };
